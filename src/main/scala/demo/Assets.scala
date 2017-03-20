ced3182827b1241275967095ef62d965a0e8eea6
David Barri <japgolly@gmail.com>
2017-03-20 16:48:36 +1100

    Fix false sense of resource safety

--- src/main/scala/demo/Assets.scala
+++ src/main/scala/demo/Assets.scala
@@ -5,7 +5,7 @@ import scalajs.js.annotation._
 
 object Assets {
 
-  @JSImport("experiment-webpack/assets", "magpie")
+  @JSImport("experiment-webpack/magpie.jpg", JSImport.Namespace)
   @js.native
   private object _magpie extends js.Any
   def magpie = _magpie.asInstanceOf[String]
