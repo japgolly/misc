85f89c2356d6d23da4bff08025bd317cc8c439ec
David Barri <japgolly@gmail.com>
2019-02-17 22:13:05 +1100

    WS jetty example

--- src/main/scala/ahh/Main.scala
+++ src/main/scala/ahh/Main.scala
@@ -5,36 +5,94 @@ import javax.servlet.http.HttpServletResponse
 import org.eclipse.jetty.server.Request
 import org.eclipse.jetty.server.Server
 import org.eclipse.jetty.server.handler.AbstractHandler
-
 import javax.websocket.server
+import org.eclipse.jetty.servlet.ServletContextHandler
+import org.eclipse.jetty.websocket.jsr356.server.deploy.WebSocketServerContainerInitializer
 
 object Main {
   def main(args: Array[String]): Unit = {
     val server = new Server(8080)
-    server.setHandler(new Handler())
+
+    // Setup the basic application "context" for this application at "/"
+    // This is also known as the handler tree (in jetty speak)
+    val context = new ServletContextHandler(ServletContextHandler.SESSIONS)
+    context.setContextPath("/")
+    server.setHandler(context)
+
+    // Initialize javax.websocket layer
+    val wscontainer = WebSocketServerContainerInitializer.configureContext(context)
+
+    // Add WebSocket endpoint to javax.websocket layer
+    wscontainer.addEndpoint(classOf[EventSocket])
+
     server.start()
+//    server.dumpStdErr()
     server.join()
   }
 }
 
-class Handler extends AbstractHandler {
+// ===========================================================================================================
 
-  override def handle(target: String,
-                      baseRequest: Request,
-                      request: HttpServletRequest,
-                      response: HttpServletResponse): Unit = {
+import javax.websocket.ClientEndpoint
+import javax.websocket.CloseReason
+import javax.websocket.OnClose
+import javax.websocket.OnError
+import javax.websocket.OnMessage
+import javax.websocket.OnOpen
+import javax.websocket.Session
+import javax.websocket.server.ServerEndpoint
 
-    // Declare response encoding and types
-    response.setContentType("text/html; charset=utf-8")
-
-    // Declare response status code
-    response.setStatus(HttpServletResponse.SC_OK)
-
-    // Write back response
-    response.getWriter().println("<h1>Hello World</h1>")
-
-    // Inform jetty that this request has now been handled
-    baseRequest.setHandled(true)
+@ClientEndpoint
+@ServerEndpoint(value = "/events/") class EventSocket {
+  @OnOpen def onWebSocketConnect(sess: Session): Unit = {
+    System.out.println("Socket Connected: " + sess)
   }
 
+  @OnMessage def onWebSocketText(message: String): Unit = {
+    System.out.println("Received TEXT message: " + message)
+  }
+
+  @OnClose def onWebSocketClose(reason: CloseReason): Unit = {
+    System.out.println("Socket Closed: " + reason)
+  }
+
+  @OnError def onWebSocketError(cause: Throwable): Unit = {
+    cause.printStackTrace(System.err)
+  }
+}
+
+// ===========================================================================================================
+
+import java.net.URI
+import javax.websocket.ContainerProvider
+import javax.websocket.Session
+import javax.websocket.WebSocketContainer
+import org.eclipse.jetty.util.component.LifeCycle
+
+object EventClient {
+  def main(args: Array[String]): Unit = {
+    val uri = URI.create("ws://localhost:8080/events/")
+    try {
+      val container = ContainerProvider.getWebSocketContainer
+      try { // Attempt Connect
+        val session = container.connectToServer(classOf[EventSocket], uri)
+        // Send a message
+        session.getBasicRemote.sendText("Hello")
+        // Close session
+        session.close()
+      } finally {
+        // Force lifecycle stop when done with container.
+        // This is to free up threads and resources that the
+        // JSR-356 container allocates. But unfortunately
+        // the JSR-356 spec does not handle lifecycles (yet)
+        container match {
+          case l: LifeCycle => l.stop()
+          case _ => ()
+        }
+      }
+    } catch {
+      case t: Throwable =>
+        t.printStackTrace(System.err)
+    }
+  }
 }
